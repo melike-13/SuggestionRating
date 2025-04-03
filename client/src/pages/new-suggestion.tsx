5c8dbf8b2ae9a4e3c4d602dfe902d7316973182e
melikekocer966 <41119809-melikekocer966@users.noreply.replit.com>
2025-04-03 14:38:30 +0000

    Restored to 'a5a239938bfc55e68435a6bc8283df180abae2fb'
    Replit-Restored-To: a5a239938bfc55e68435a6bc8283df180abae2fb

--- client/src/pages/new-suggestion.tsx
+++
@@ -1,9 +0,0 @@
-import { SuggestionForm } from "@/components/suggestions/SuggestionForm";
-
-export default function NewSuggestion() {
-  return (
-    <div className="max-w-4xl mx-auto">
-      <SuggestionForm />
-    </div>
-  );
-}
